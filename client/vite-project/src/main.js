6a39c331026a93d5ab0c22b88d826e291123c024
yukishi24 <yukishi24github@gmail.com>
2023-04-16 00:07:36 -0700

    modify: main.js

--- client/vite-project/src/main.js
+++ client/vite-project/src/main.js
@@ -1,10 +1,21 @@
+// import { createApp } from 'vue'
+// import App from './App.vue'
+// import vuetify from './plugins/vuetify'
+// import { loadFonts } from './plugins/webfontloader'
+
+// loadFonts()
+
+// createApp(App)
+//   .use(vuetify)
+//   .mount('#app')
+
 import { createApp } from 'vue'
-import App from './App.vue'
+import Home from './Home.vue'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
 
 loadFonts()
 
-createApp(App)
+createApp(Home)
   .use(vuetify)
   .mount('#app')
