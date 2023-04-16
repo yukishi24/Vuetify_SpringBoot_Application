1cf8718bf269008f8bc37475ca201a2f707048dd
yukishi24 <yukishi24github@gmail.com>
2023-04-16 22:53:40 +0900

    modify: Home.vue

--- client/vite-project/src/Home.vue
+++ client/vite-project/src/Home.vue
@@ -2,7 +2,7 @@
     <v-app>
         <v-card class="rounded-0">
             <v-toolbar color="primary" flat>
-                <v-btn icon="mdi-menu" @click="getPeople()"></v-btn>
+                <v-btn icon="mdi-menu"></v-btn>
                     社員一覧
             </v-toolbar>
             <div>{{ people }}</div>
@@ -20,11 +20,11 @@ export default{
     },
     methods:{
         getPeople(){
-            const url = "http://localhost:8080/home"
-            axios.get(url)
+            const url = axios.get("http://localhost:8080/home")
                 .then(response => {this.people = response.data})
                 .catch(error => {console.log(error)})
         }
     },
+    created(){this.getPeople()}
 }
 </script>
